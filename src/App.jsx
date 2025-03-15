9f5bc057f33adbc2a84768b46081093e7695edef
elliot crowder <elliot.k.crowder@gmail.com>
2025-03-15 13:55:48 -0700

    refactoring point generation to do conversion to pixel points in function in order to add null points at asymptotes by detecting jumpes across the entire screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import * as math from "mathjs";
 import "./App.css";
 import "./cartesianStyles.css";
 import {
-	drawAxes,
-	plotFunction,
-	generatePoints,
-	mapPointsToPixels,
+  drawAxes,
+  plotFunction,
+  generatePoints,
+  mapPointsToPixels,
 } from "./utils/draw";
 // function isValidExpression(expression) {
 // 	const variablePattern = /(?:^|[^a-zA-Z])[a-wyz](?:$|[^a-zA-Z])/i;
@@ -31,305 +31,319 @@ import {
 // }
 
 function isValidExpression(expression) {
-	try {
-		math.evaluate(expression, { x: 1 });
-		return true;
-	} catch (err) {
-		return false;
-	}
+  try {
+    math.evaluate(expression, { x: 1 });
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 function App() {
-	const [inputXmin, setInputXmin] = useState("-10");
-	const [inputXmax, setInputXmax] = useState("+10");
-	const [inputYmin, setInputYmin] = useState("-10");
-	const [inputYmax, setInputYmax] = useState("+10");
+  const [inputXmin, setInputXmin] = useState("-10");
+  const [inputXmax, setInputXmax] = useState("+10");
+  const [inputYmin, setInputYmin] = useState("-10");
+  const [inputYmax, setInputYmax] = useState("+10");
 
-	const [xmin, setXmin] = useState("-10");
-	const [xmax, setXmax] = useState("+10");
-	const [ymin, setYmin] = useState("-10");
-	const [ymax, setYmax] = useState("+10");
+  const [xmin, setXmin] = useState("-10");
+  const [xmax, setXmax] = useState("+10");
+  const [ymin, setYmin] = useState("-10");
+  const [ymax, setYmax] = useState("+10");
 
-	const [error, setError] = useState(false);
-	const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-	const [inputExpr, setInputExpr] = useState("");
-	const [expr, setExpr] = useState("");
+  const [inputExpr, setInputExpr] = useState("");
+  const [expr, setExpr] = useState("");
 
-	const canvasRef = useRef();
+  const canvasRef = useRef();
 
-	function handleInputExpression(event) {
-		const expression = event.target.value;
+  function handleInputExpression(event) {
+    const expression = event.target.value;
 
-		setInputExpr(expression);
-	}
-	function handleXmin(event) {
-		setInputXmin(event.target.value);
-	}
+    setInputExpr(expression);
+  }
+  function handleXmin(event) {
+    setInputXmin(event.target.value);
+  }
 
-	function handleXmax(event) {
-		setInputXmax(event.target.value);
-	}
+  function handleXmax(event) {
+    setInputXmax(event.target.value);
+  }
 
-	function handleYmin(event) {
-		setInputYmin(event.target.value);
-	}
+  function handleYmin(event) {
+    setInputYmin(event.target.value);
+  }
 
-	function handleYmax(event) {
-		setInputYmax(event.target.value);
-	}
-	useEffect(() => {
-		if (!inputExpr) {
-			setError(true);
-			setErrorMessage("empty expression field");
-			return;
-		}
-		if (!isValidExpression(inputExpr)) {
-			setError(true);
-			setErrorMessage("Invalid Expression");
-			return;
-		}
-		try {
-			math.compile(inputExpr);
-			setError(false);
-			setExpr(inputExpr);
-			setErrorMessage("");
-		} catch (err) {
-			setErrorMessage("invalid expr");
-			setError(true);
-		}
-	}, [inputExpr]);
-	useEffect(() => {
-		if (!inputXmin || !inputXmax || !inputYmin || !inputYmax) {
-			setError(true);
-			setErrorMessage("at least one field left empty");
-			return;
-		}
-		if (
-			isNaN(Number(inputXmin)) ||
-			isNaN(Number(inputXmax)) ||
-			isNaN(Number(inputYmin)) ||
-			isNaN(Number(inputYmax))
-		) {
-			setError(true);
-			setErrorMessage("no number");
-			return;
-		}
-		if (Number(inputXmin) > Number(inputXmax)) {
-			setError(true);
-			setErrorMessage("xmin > xmax");
-			return;
-		}
-		if (Number(inputYmin) > Number(inputYmax)) {
-			setError(true);
-			setErrorMessage("ymin > ymax");
-			return;
-		}
-		console.log("pizza");
-		setXmin(inputXmin);
-		setXmax(inputXmax);
-		setYmin(inputYmin);
-		setYmax(inputYmax);
-		setError(false);
-		setErrorMessage("");
-	}, [inputXmax, inputXmin, inputYmax, inputYmin]);
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		if (!canvas || error) return;
-		const ctx = canvas.getContext("2d");
-		ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear before redrawing
-		console.log(typeof xmin);
-		drawAxes(ctx, xmin, xmax, ymin, ymax, 300, 500);
-		//move function plotting somewhere else once input is set up
-		const points = Array.from(
-			generatePoints(
-				expr,
-				parseInt(xmax), //change these to Number(parseInt)?
-				parseInt(xmin),
-				parseInt(ymax),
-				parseInt(ymin)
-			)
-		);
-		console.log(points);
-		const pixelPoints = mapPointsToPixels(
-			points,
-			xmin,
-			xmax,
-			ymin,
-			ymax,
-			300,
-			500
-		);
-		console.log("pixpoints:", pixelPoints);
-		plotFunction(pixelPoints, 500, 300, ctx);
-	}, [xmin, xmax, ymin, ymax, expr]); // Now updates when state changes
-	return (
-		<div>
-			<input type="text" value={inputExpr} onChange={handleInputExpression} />
-			<GraphWindowInputFields
-				xmin={inputXmin}
-				xmax={inputXmax}
-				ymin={inputYmin}
-				ymax={inputYmax}
-				handleXmin={handleXmin}
-				handleXmax={handleXmax}
-				handleYmin={handleYmin}
-				handleYmax={handleYmax}
-			/>
-			<div>{errorMessage}</div>
-			<div>{JSON.stringify(error)}</div>
-			<CartesianGrid
-				expr={expr}
-				xmin={xmin}
-				xmax={xmax}
-				ymin={ymin}
-				ymax={ymax}
-				canvasRef={canvasRef}
-			/>
-		</div>
-	);
+  function handleYmax(event) {
+    setInputYmax(event.target.value);
+  }
+  useEffect(() => {
+    if (!inputExpr) {
+      setError(true);
+      setErrorMessage("empty expression field");
+      return;
+    }
+    if (!isValidExpression(inputExpr)) {
+      setError(true);
+      setErrorMessage("Invalid Expression");
+      return;
+    }
+    try {
+      math.compile(inputExpr);
+      setError(false);
+      setExpr(inputExpr);
+      setErrorMessage("");
+    } catch (err) {
+      setErrorMessage("invalid expr");
+      setError(true);
+    }
+  }, [inputExpr]);
+  useEffect(() => {
+    if (!inputXmin || !inputXmax || !inputYmin || !inputYmax) {
+      setError(true);
+      setErrorMessage("at least one field left empty");
+      return;
+    }
+    if (
+      isNaN(Number(inputXmin)) ||
+      isNaN(Number(inputXmax)) ||
+      isNaN(Number(inputYmin)) ||
+      isNaN(Number(inputYmax))
+    ) {
+      setError(true);
+      setErrorMessage("no number");
+      return;
+    }
+    if (Number(inputXmin) > Number(inputXmax)) {
+      setError(true);
+      setErrorMessage("xmin > xmax");
+      return;
+    }
+    if (Number(inputYmin) > Number(inputYmax)) {
+      setError(true);
+      setErrorMessage("ymin > ymax");
+      return;
+    }
+    console.log("pizza");
+    setXmin(inputXmin);
+    setXmax(inputXmax);
+    setYmin(inputYmin);
+    setYmax(inputYmax);
+    setError(false);
+    setErrorMessage("");
+  }, [inputXmax, inputXmin, inputYmax, inputYmin]);
+  const canvasScaleFactor = 4;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas || error) return;
+    const ctx = canvas.getContext("2d");
+
+    //set internal canvas resolution higher than displayed size
+    canvas.width = 500 * canvasScaleFactor;
+    canvas.height = 300 * canvasScaleFactor;
+
+    canvas.style.width = "500px";
+    canvas.style.height = "300px";
+    ctx.scale(canvasScaleFactor, canvasScaleFactor);
+    // Clear before redrawing
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(typeof xmin);
+
+    drawAxes(ctx, xmin, xmax, ymin, ymax, 300, 500); //draw axes
+
+    const points = Array.from(
+      generatePoints(
+        expr,
+        parseInt(xmax), //change these to Number(parseInt)?
+        parseInt(xmin),
+        parseInt(ymax),
+        parseInt(ymin)
+      )
+    );
+    console.log(points);
+    const pixelPoints = mapPointsToPixels(
+      points,
+      xmin,
+      xmax,
+      ymin,
+      ymax,
+      300,
+      500
+    );
+    // ctx.lineWidth = 1 / canvasScaleFactor;
+    // ctx.lineJoin = "round";
+    // ctx.lineCap = "round";
+    console.log("pixpoints:", pixelPoints);
+    plotFunction(pixelPoints, 500, 300, ctx);
+  }, [xmin, xmax, ymin, ymax, expr]); // Now updates when state changes
+  return (
+    <div>
+      <input type="text" value={inputExpr} onChange={handleInputExpression} />
+      <GraphWindowInputFields
+        xmin={inputXmin}
+        xmax={inputXmax}
+        ymin={inputYmin}
+        ymax={inputYmax}
+        handleXmin={handleXmin}
+        handleXmax={handleXmax}
+        handleYmin={handleYmin}
+        handleYmax={handleYmax}
+      />
+      <div>{errorMessage}</div>
+      <div>{JSON.stringify(error)}</div>
+      <CartesianGrid
+        expr={expr}
+        xmin={xmin}
+        xmax={xmax}
+        ymin={ymin}
+        ymax={ymax}
+        canvasRef={canvasRef}
+      />
+    </div>
+  );
 }
 
 function CartesianGrid({ xmin, xmax, ymin, ymax, canvasRef }) {
-	const graphWidth = 500;
-	const graphHeight = 300;
+  const graphWidth = 500;
+  const graphHeight = 300;
 
-	function handleClick(event) {
-		const canvas = canvasRef.current;
-		const draw = canvas.getContext("2d");
-		const offsetX = event.nativeEvent.offsetX;
-		const offsetY = event.nativeEvent.offsetY;
-		const cursorCoord = {
-			x: offsetX,
-			y: offsetY,
-		};
-		// console.log(cursorCoord);
-		draw.beginPath();
-		draw.arc(cursorCoord.x, cursorCoord.y, 40, 0, 2 * Math.PI);
-		draw.fillStyle = "green";
-		draw.fill();
-	}
+  function handleClick(event) {
+    const canvas = canvasRef.current;
+    const draw = canvas.getContext("2d");
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+    const cursorCoord = {
+      x: offsetX,
+      y: offsetY,
+    };
+    // console.log(cursorCoord);
+    draw.beginPath();
+    draw.arc(cursorCoord.x, cursorCoord.y, 40, 0, 2 * Math.PI);
+    draw.fillStyle = "green";
+    draw.fill();
+  }
 
-	function handleMouseOver() {
-		const canvas = canvasRef.current;
-		canvas.style.outline = "2px solid green";
-	}
+  function handleMouseOver() {
+    const canvas = canvasRef.current;
+    canvas.style.outline = "2px solid green";
+  }
 
-	function handleMouseOut() {
-		const canvas = canvasRef.current;
-		canvas.style.outline = "1px solid black";
-	}
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		const draw = canvas.getContext("2d");
-		draw.lineCap = "round";
-		draw.fillStyle = "black";
-		draw.font = "18px";
-		drawAxes(draw, xmin, xmax, ymin, ymax, graphHeight, graphWidth);
-	}, []);
-	return (
-		<div>
-			<div>
-				<pre>{JSON.stringify([xmin, xmax, ymin, ymax])}</pre>
-			</div>
-			<canvas
-				width={graphWidth + "px"}
-				height={graphHeight + "px"}
-				className="cartesian-canvas"
-				ref={canvasRef}
-				// onMouseMove={handleMouseMove}
-				onMouseOver={handleMouseOver}
-				onMouseOut={handleMouseOut}
-				onClick={handleClick}
-			/>
-		</div>
-	);
+  function handleMouseOut() {
+    const canvas = canvasRef.current;
+    canvas.style.outline = "1px solid black";
+  }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const draw = canvas.getContext("2d");
+    draw.lineCap = "round";
+    draw.fillStyle = "black";
+    draw.font = "18px";
+    drawAxes(draw, xmin, xmax, ymin, ymax, graphHeight, graphWidth);
+  }, []);
+  return (
+    <div>
+      <div>
+        <pre>{JSON.stringify([xmin, xmax, ymin, ymax])}</pre>
+      </div>
+      <canvas
+        width={graphWidth + "px"}
+        height={graphHeight + "px"}
+        className="cartesian-canvas"
+        ref={canvasRef}
+        // onMouseMove={handleMouseMove}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleClick}
+      />
+    </div>
+  );
 }
 
 function GraphWindowInputFields({
-	xmin,
-	xmax,
-	ymin,
-	ymax,
-	handleYmax,
-	handleYmin,
-	handleXmin,
-	handleXmax,
+  xmin,
+  xmax,
+  ymin,
+  ymax,
+  handleYmax,
+  handleYmin,
+  handleXmin,
+  handleXmax,
 }) {
-	// const yMinRef = useRef();
-	// const yMaxRef = useRef();
+  // const yMinRef = useRef();
+  // const yMaxRef = useRef();
 
-	function logXRange() {
-		const xRange = {
-			xmin: xmin,
-			xmax: xmax,
-		};
-		console.log(xRange);
-	}
+  function logXRange() {
+    const xRange = {
+      xmin: xmin,
+      xmax: xmax,
+    };
+    console.log(xRange);
+  }
 
-	function logYRange() {
-		const yRange = {
-			ymin: ymin,
-			ymax: ymax,
-		};
-		console.dir(yRange);
-	}
+  function logYRange() {
+    const yRange = {
+      ymin: ymin,
+      ymax: ymax,
+    };
+    console.dir(yRange);
+  }
 
-	function handleKeyDown(event) {
-		console.log(event.key);
-		if (event.key === "Enter") {
-			alert("Enter key press");
-		}
-	}
+  function handleKeyDown(event) {
+    console.log(event.key);
+    if (event.key === "Enter") {
+      alert("Enter key press");
+    }
+  }
 
-	useEffect(() => {
-		setTimeout(() => {
-			console.log("banana");
-		}, 800);
-	}, []);
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("banana");
+    }, 800);
+  }, []);
 
-	return (
-		<div>
-			<div>
-				<span className="input-field-container">
-					<input
-						type="text"
-						placeholder="x-min"
-						value={xmin}
-						onChange={handleXmin}
-						onKeyDown={handleKeyDown}
-					/>
-				</span>
-				<span className="input-field-container">
-					<input
-						type="text"
-						placeholder="x-max"
-						value={xmax}
-						onChange={handleXmax}
-					/>
-				</span>
-				<button onClick={logXRange}>log x range</button>
-			</div>
-			<div>
-				<span className="input-field-container">
-					<input
-						type="text"
-						placeholder="y-min"
-						onChange={handleYmin}
-						value={ymin}
-					/>
-				</span>
-				<span className="input-field-container">
-					<input
-						type="text"
-						placeholder="y-max"
-						onChange={handleYmax}
-						value={ymax}
-					/>
-				</span>
+  return (
+    <div>
+      <div>
+        <span className="input-field-container">
+          <input
+            type="text"
+            placeholder="x-min"
+            value={xmin}
+            onChange={handleXmin}
+            onKeyDown={handleKeyDown}
+          />
+        </span>
+        <span className="input-field-container">
+          <input
+            type="text"
+            placeholder="x-max"
+            value={xmax}
+            onChange={handleXmax}
+          />
+        </span>
+        <button onClick={logXRange}>log x range</button>
+      </div>
+      <div>
+        <span className="input-field-container">
+          <input
+            type="text"
+            placeholder="y-min"
+            onChange={handleYmin}
+            value={ymin}
+          />
+        </span>
+        <span className="input-field-container">
+          <input
+            type="text"
+            placeholder="y-max"
+            onChange={handleYmax}
+            value={ymax}
+          />
+        </span>
 
-				<button onClick={logYRange}>log y range</button>
-			</div>
-		</div>
-	);
+        <button onClick={logYRange}>log y range</button>
+      </div>
+    </div>
+  );
 }
 export default App;
--- src/utils/draw.js
+++ src/utils/draw.js
@@ -4,164 +4,180 @@ import { evaluateExpression } from "./parsing/functionParse.js";
 //the function
 //ths should be in draw, other functons need to be moved to another util file
 export function plotFunction(points, winWidth, winHeight, ctx) {
-	if (points.length === 0) {
-		return;
-	}
-	ctx.beginPath();
-	const sortedPoints = points.sort((a, b) => a.xPxCoord - b.xPxCoord);
+  if (points.length === 0) {
+    return;
+  }
+  // ctx.beginPath();
+  const path = new Path2D();
+  const sortedPoints = points.sort((a, b) => a.xPxCoord - b.xPxCoord);
 
-	let isPrevOutOfBounds = false; //flag for discontinuous points
+  let isPrevOutOfBounds = false; //flag for discontinuous points
 
-	ctx.moveTo(sortedPoints[0].xPxCoord, sortedPoints[0].yPxCoord);
-	for (const point of sortedPoints) {
-		if (point.yPxCoord < 0 || point.yPxCoord > winHeight) {
-			isPrevOutOfBounds = true;
-			continue;
-		} else {
-			if (isPrevOutOfBounds) {
-				ctx.moveTo(point.xPxCoord, point.yPxCoord);
-				isPrevOutOfBounds = false;
-			} else {
-				ctx.lineTo(point.xPxCoord, point.yPxCoord);
-			}
-		}
-	}
-	ctx.stroke();
+  ctx.moveTo(sortedPoints[0].xPxCoord, sortedPoints[0].yPxCoord);
+  for (const point of sortedPoints) {
+    if (point.yPxCoord < 0 || point.yPxCoord > winHeight) {
+      isPrevOutOfBounds = true;
+      continue;
+    } else {
+      if (isPrevOutOfBounds) {
+        // ctx.moveTo(point.xPxCoord, point.yPxCoord);
+        path.moveTo(point.xPxCoord, point.yPxCoord);
+        isPrevOutOfBounds = false;
+      } else {
+        // ctx.lineTo(point.xPxCoord, point.yPxCoord);
+        path.lineTo(point.xPxCoord, point.yPxCoord);
+      }
+    }
+  }
+  ctx.stroke(path);
 }
-export function generatePoints(expr, xmax, xmin, ymax, ymin, res = 0.01) {
-	if (!expr) {
-		return [];
-	}
+export function generatePoints(
+  expr,
+  xmax,
+  xmin,
+  ymax,
+  ymin,
+  winWidth,
+  winHeight
+) {
+  if (!expr) {
+    return []; //if there is no expression there are no points to plot
+  }
+  const jumpThreshold = winHeight * 0.95; //the maximum distance between two points
+  const domain = xmax - xmin;
 
-	const maxPoints = 10000;
-	// res = (xmax - xmin) / 200;
-	res = Math.max(res, (xmax - xmin) / maxPoints);
-	let points = new Set([]);
-	// let prevInBounds = false;
-	// let prevPoint = null;
+  const numPoints = 1000;
+  const step = domain / 1000;
 
-	for (let x = xmin; x <= xmax; x += res) {
-		let y = evaluateExpression(expr, "x", x).y;
-		// let inBounds = y >= ymin && y <= ymax;
+  let points = new Set([]);
 
-		// if (inBounds) {
-		// 	if (prevInBounds && prevPoint) {
-		// 		continue;
-		// 	}
-		// }
-		// if (y >= ymin && y <= ymax) {
-		// Ensure y is within bounds
-		points.add({ x, y });
-		// }
-	}
-	return points;
+  for (let i = xmin; i <= xmax; i += step) {
+    let y = evaluateExpression(expr, "x", x).y; //the cartesian y coordinate
+    let realPoint = { x: i, y: y }; //the real cartesian coordinates
+
+    let pxCoords = mapPointToPixels(
+      realPoint,
+      ymin,
+      ymax,
+      xmin,
+      xmax,
+      winHeight,
+      winWidth
+    ); //cartesian coordinates mapped into the pixel space
+
+    const yPxCoord = pxCoords.yPxCoord;
+    const xPxCoord = pxCoords.xPxCoord;
+
+    points.add({ x, y });
+  }
+  return points;
 }
 
 export function drawAxes(
-	ctx,
-	xmin,
-	xmax,
-	ymin,
-	ymax,
-	canvasHeight,
-	canvasWidth
+  ctx,
+  xmin,
+  xmax,
+  ymin,
+  ymax,
+  canvasHeight,
+  canvasWidth
 ) {
-	ctx.strokeStyle = "black";
-	ctx.lineWidth = 1.5;
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 1.5;
 
-	//get origin points in pixel form
-	const pxPoint = mapPointToPixels(
-		{ x: 0, y: 0 },
-		parseInt(ymin),
-		parseInt(ymax),
-		parseInt(xmin),
-		parseInt(xmax),
-		canvasHeight,
-		canvasWidth
-	);
-	const xOrigin = pxPoint.xPxCoord;
-	const yOrigin = pxPoint.yPxCoord;
-	//draw x axis
-	ctx.beginPath();
-	ctx.moveTo(0, yOrigin);
-	ctx.lineTo(canvasWidth, yOrigin);
+  //get origin points in pixel form
+  const pxPoint = mapPointToPixels(
+    { x: 0, y: 0 },
+    parseInt(ymin),
+    parseInt(ymax),
+    parseInt(xmin),
+    parseInt(xmax),
+    canvasHeight,
+    canvasWidth
+  );
+  const xOrigin = pxPoint.xPxCoord;
+  const yOrigin = pxPoint.yPxCoord;
+  //draw x axis
+  ctx.beginPath();
+  ctx.moveTo(0, yOrigin);
+  ctx.lineTo(canvasWidth, yOrigin);
 
-	//draw y axis
-	ctx.moveTo(xOrigin, 0);
-	ctx.lineTo(xOrigin, canvasHeight);
-	ctx.stroke();
+  //draw y axis
+  ctx.moveTo(xOrigin, 0);
+  ctx.lineTo(xOrigin, canvasHeight);
+  ctx.stroke();
 }
 
 export function drawGrid(winHeight, winWidth) {
-	for (let i = 0; i < winWidth; i += 20) {
-		const coord1 = { x: i, y: 0 };
-		const coord2 = { x: i, y: winHeight };
-		drawGridLine(coord1, coord2);
-	}
-	for (let i = 0; i < winHeight; i += 20) {
-		const coord1 = { x: 0, y: i };
-		const coord2 = { x: winWidth, y: i };
-		drawGridLine(coord1, coord2);
-	}
+  for (let i = 0; i < winWidth; i += 20) {
+    const coord1 = { x: i, y: 0 };
+    const coord2 = { x: i, y: winHeight };
+    drawGridLine(coord1, coord2);
+  }
+  for (let i = 0; i < winHeight; i += 20) {
+    const coord1 = { x: 0, y: i };
+    const coord2 = { x: winWidth, y: i };
+    drawGridLine(coord1, coord2);
+  }
 }
 
 function drawGridLine(coord1, coord2, draw) {
-	draw.strokeStyle = "grey";
-	draw.beginPath();
-	draw.moveTo(coord1.x, coord1.y);
-	draw.lineTo(coord2.x, coord2.y);
-	draw.stroke();
-	draw.font = "50px Arial";
-	// draw.fillText("Hello World", coord1.x, coord1.y);
+  draw.strokeStyle = "grey";
+  draw.beginPath();
+  draw.moveTo(coord1.x, coord1.y);
+  draw.lineTo(coord2.x, coord2.y);
+  draw.stroke();
+  draw.font = "50px Arial";
+  // draw.fillText("Hello World", coord1.x, coord1.y);
 }
 export function mapXCoordToPx(x, xMin, xMax, canvasWidth) {
-	const xPx = ((x - xMin) / (xMax - xMin)) * canvasWidth;
-	return Math.trunc(xPx);
+  const xPx = ((x - xMin) / (xMax - xMin)) * canvasWidth;
+  return Math.trunc(xPx);
 }
 
 export function mapYCoordToPx(y, yMin, yMax, canvasHeight) {
-	const yPx = canvasHeight - ((y - yMin) / (yMax - yMin)) * canvasHeight;
-	return Math.trunc(yPx);
+  const yPx = canvasHeight - ((y - yMin) / (yMax - yMin)) * canvasHeight;
+  return Math.trunc(yPx);
 }
 
 export function mapPointToPixels(
-	point,
-	yMin,
-	yMax,
-	xMin,
-	xMax,
-	canvasHeight,
-	canvasWidth
+  point,
+  yMin,
+  yMax,
+  xMin,
+  xMax,
+  canvasHeight,
+  canvasWidth
 ) {
-	const yPxCoord = mapYCoordToPx(point.y, yMin, yMax, canvasHeight);
-	const xPxCoord = mapXCoordToPx(point.x, xMin, xMax, canvasWidth);
-	const pxPoint = {
-		yPxCoord,
-		xPxCoord,
-	};
-	return pxPoint;
+  const yPxCoord = mapYCoordToPx(point.y, yMin, yMax, canvasHeight);
+  const xPxCoord = mapXCoordToPx(point.x, xMin, xMax, canvasWidth);
+  const pxPoint = {
+    yPxCoord,
+    xPxCoord,
+  };
+  return pxPoint;
 }
 
 export function mapPointsToPixels(
-	points,
-	xMin,
-	xMax,
-	yMin,
-	yMax,
-	canvasHeight,
-	canvasWidth
+  points,
+  xMin,
+  xMax,
+  yMin,
+  yMax,
+  canvasHeight,
+  canvasWidth
 ) {
-	return points.map((point) => {
-		return mapPointToPixels(
-			point,
-			yMin,
-			yMax,
-			xMin,
-			xMax,
-			canvasHeight,
-			canvasWidth
-		);
-	});
+  return points.map((point) => {
+    return mapPointToPixels(
+      point,
+      yMin,
+      yMax,
+      xMin,
+      xMax,
+      canvasHeight,
+      canvasWidth
+    );
+  });
 }
 
 //tests
@@ -190,12 +206,12 @@ console.log(mapXCoordToPx(x, xMin, xMax, canvasWidth));
 console.log(mapYCoordToPx(y, yMin, yMax, canvasHeight));
 
 const point = {
-	x: 5,
-	y: 5,
+  x: 5,
+  y: 5,
 };
 
 console.log(
-	mapPointToPixels(point, yMin, yMax, xMin, xMax, canvasHeight, canvasWidth)
+  mapPointToPixels(point, yMin, yMax, xMin, xMax, canvasHeight, canvasWidth)
 );
 
 const points = generatePoints("x^2", 10, -10, 10, -10);
